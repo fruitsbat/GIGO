ab1d55100e135e14e229c0c7532850270268c65e
zoe <ZoeOosting@outlook.com>
2023-05-24 13:53:04 +0200

    ewewewewe

--- src/app/audiocalc/audiocalc.component.ts
+++ src/app/audiocalc/audiocalc.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { formatNumber } from '@angular/common';
 
 @Component({
   selector: 'app-audiocalc',
@@ -7,11 +8,21 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./audiocalc.component.scss']
 })
 export class AudiocalcComponent {
-  salary = new FormControl(1, {nonNullable: true});
-  hours = new FormControl(1, {nonNullable: true});
-  transcriptionistSpeed = new FormControl(2, {nonNullable: true});
+  salary = new FormControl(7, {nonNullable: true});
+  hours = new FormControl(1000, {nonNullable: true});
+  transcriptionistSpeed = new FormControl(3, {nonNullable: true});
 
-  getTotalMoney(): number {
-    return this.salary.value + this.hours.value;
+  formatAudioLength(): string {
+    return formatNumber(this.hours.value, "EN");
+  }
+
+  getTotalMoney(): string {
+    let v = formatNumber(this.hours.value * this.salary.value * this.transcriptionistSpeed.value, "EN");
+    return v;
+  }
+
+  getTotalTimeSpent(): string {
+    let v = formatNumber(this.hours.value * this.transcriptionistSpeed.value, "EN");
+    return v;
   }
 }
