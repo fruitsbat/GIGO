378232bf8a79114742230f07759f5aa902c231f4
zoe <ZoeOosting@outlook.com>
2023-05-24 19:17:41 +0200

    finish speech recognition

--- src/app/audiocalc/audiocalc.component.ts
+++ src/app/audiocalc/audiocalc.component.ts
@@ -9,9 +9,9 @@ import { SliderData } from '../sliderdata';
   styleUrls: ['./audiocalc.component.scss']
 })
 export class AudiocalcComponent {
-  salary = new FormControl(20.25, {nonNullable: true});
-  hours = new FormControl(1791.0, {nonNullable: true});
-  transcriptionistSpeed = new FormControl(3.0, {nonNullable: true});
+  salary = new FormControl(20.25, { nonNullable: true });
+  hours = new FormControl(1791.0, { nonNullable: true });
+  transcriptionistSpeed = new FormControl(3.0, { nonNullable: true });
 
   salaries: SliderData[] = [
     {
@@ -46,8 +46,65 @@ export class AudiocalcComponent {
     }
   ];
 
+  speeds: SliderData[] = [
+    {
+      value: 2.5,
+      short: "professional transcriptionist",
+      href: "https://www.notta.ai/en/blog/how-long-does-it-take-to-transcribe-1-hour-of-audio",
+      about: "a professional transcriptionist can transcribe one hour of audio in 2 to 3 hours",
+    },
+    {
+      value: 10,
+      short: "amateur transcriptionist",
+      href: "https://www.notta.ai/en/blog/how-long-does-it-take-to-transcribe-1-hour-of-audio",
+      about: "an amatuer transcriptionist can use up to ten hours to transcribe one hour of audio",
+    }
+  ]
+
+  dataSets: SliderData[] = [
+    {
+      value: 100,
+      short: "AMI",
+      href: "https://huggingface.co/datasets/edinburghcstr/ami",
+      about: "AMI is a data set of recorded business meetings",
+    },
+    {
+      value: 1791,
+      short: "VoxPopuli",
+      href: "https://huggingface.co/datasets/facebook/voxpopuli",
+      about: "VoxPopuli is a set of transcribed audio from the eu parliament"
+    },
+    {
+      value: 2763,
+      short: "AudioSet",
+      href: "https://research.google.com/audioset/dataset/speech.html",
+      about: "AudioSet is a data set taken from google captions",
+    },
+    {
+      value: 3209,
+      short: "Common Voice 13 (english)",
+      href: "https://commonvoice.mozilla.org/en/datasets",
+      about: "Mozilla Common Voice is a dataset recorded and verified by volunteers"
+    },
+    {
+      value: 17689,
+      short: "Common Voice 13 (full, validated)",
+      href: "https://huggingface.co/datasets/mozilla-foundation/common_voice_13_0",
+      about: "Mozilla Common Voice is a dataset recorded and verified by volunteers"
+    },
+  ];
+
+
+  getSelectedHours(): SliderData[] {
+    return this.dataSets.filter(s => s.value === this.hours.value);
+  }
+
   getSelectedSalaries(): SliderData[] {
     return this.salaries.filter(s => s.value === this.salary.value);
+  }
+
+  getSelectedSpeeds(): SliderData[] {
+    return this.speeds.filter(s => s.value === this.transcriptionistSpeed.value);
   }
 
   formatAudioLength(): string {
